e13c9a50d08e761744157cb5115826f8dde34dd4
Fee Elders <fee.elders@gmail.com>
2024-06-03 15:59:31 +0200

    beginning code for station and connection classes

---
+++ railways.py
@@ -0,0 +1,10 @@
+class Station():
+    def __init__(self) -> None:
+        # to do: read csv files with station coördinates
+
+
+class Connection():
+    """ Object for connections between two stations. """
+    def __init__(self) -> None:
+        # to do: read csv file with connections
+
