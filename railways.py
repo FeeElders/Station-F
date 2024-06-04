e497f3ca0e549a7bd04b4e41d2b20d4722e9540b
Fee Elders <fee.elders@gmail.com>
2024-06-04 16:00:23 +0200

    organize files for station classes

--- railways.py
+++ railways.py
@@ -1,10 +1,1 @@
-class Station():
-    def __init__(self) -> None:
-        # to do: read csv files with station coördinates
-
-
-class Connection():
-    """ Object for connections between two stations. """
-    def __init__(self) -> None:
-        # to do: read csv file with connections
-
+from stations import Station, Connection
---
+++ stations.py
@@ -0,0 +1,21 @@
+class Station():
+    def __init__(self, name: str, x: float, y: float) -> None:
+        # to do: read csv files with station coördinates
+        self.name = name
+        self.x = x
+        self.y = y
+
+        self.connections: dict[int: 'Station'] = {}
+
+    def add_csv() -> None:
+        """ add the stations from the csv file """
+
+    def add_station() - None:
+        
+
+
+class Connection():
+    """ Object for connections between two stations. """
+    def __init__(self) -> None:
+        # to do: read csv file with connections
+
