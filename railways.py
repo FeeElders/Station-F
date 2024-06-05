32880c7c4511a37e2cbe52f8b436019176034172
Femkeottow <165044421+Femkeottow@users.noreply.github.com>
2024-06-05 10:33:00 +0200

    brainstorm representation

--- railways.py
+++ railways.py
@@ -1,1 +1,19 @@
 from stations import Station, Connection
+
+
+
+
+print("WELCOME TO RailNL")
+# Begin met een random station
+all_stations = Station()
+time = 120 #max minutes
+
+
+# run 7 trajectories
+while True:
+    station = all_stations.get_station()
+    traject = Connection(station, time)
+    
+    # allow adding stations as long as the time is not empty
+    while traject.is_running():
+        traject.add_connection()
--- stations.py
+++ stations.py
@@ -1,3 +1,6 @@
+"""make sure random function can be used"""
+import random
+
 class Station():
     def __init__(self, name: str, x: float, y: float) -> None:
         # to do: read csv files with station coördinates
@@ -10,12 +13,61 @@ class Station():
     def add_csv() -> None:
         """ add the stations from the csv file """
 
-    def add_station() - None:
+    def add_station() -> None:
+        
+    def get_station() -> str:
+        """
+        Choose a random station from the dictionary/list with stations.
+        """
+        return random.choice(self.stations)
         
 
 
-class Connection():
+class Connection(self, station: str, time: int):
     """ Object for connections between two stations. """
     def __init__(self) -> None:
         # to do: read csv file with connections
+        self.station = station
+        self.time = time
+        self.connections: dict[int: 'Station'] = {}
+        file = open('ConnectiesHolland.csv', "r")
+        for line in file:
+            # Save all possible connections in a dict
+
+        file.close()
+        
+    
+    def is_running(self) -> bool:
+        """check if still enough time left"""  
+        
+    def add_connection(self) -> str:
+        """ 
+        search for a connection with the given station. 
+        Give back the onther station in the connection 
+        make sure to delete the travel time from the total time 
+        """  
+        for line in self.connections:
+            if self.station in line: # and if time in line below total time
+                # add to possebillities 
+                
+    def passed(self) -> str:
+        """
+        list of all stations that are passed
+        """   
+        
+        
+        
+    def pattern(self) -> str:
+        """ 
+        Save the connections
+        When the track is complete give back all the connections
+        Containing start and finish of each connection and the time passed 
+        """
+            
+            
+    def score(self) -> int:
+        # K = p*10000 - (T*100 + Min)
+        
+        
+        
 
