abcf1f9416a78f057103581adbc1e4ba854c0202
Fee Elders <fee.elders@gmail.com>
2024-06-05 13:28:45 +0200

    working code for connections with string

--- railways.py
+++ railways.py
@@ -1,19 +1,27 @@
-from stations import Station, Connection
+from stations import Station, Planning, Connection
 
 
+if __name__ == "__main__":
+    print("WELCOME TO RailNL")
 
-
-print("WELCOME TO RailNL")
-# Begin met een random station
-all_stations = Station()
-time = 120 #max minutes
-
-
-# run 7 trajectories
-while True:
-    station = all_stations.get_station()
-    traject = Connection(station, time)
+    stations = Planning()
     
-    # allow adding stations as long as the time is not empty
-    while traject.is_running():
-        traject.add_connection()
+    stations.print_stations()
+
+    stations.get_connections("Amsterdam Sloterdijk")
+
+    stations.print_all_connections()
+    
+    # # Begin met een random station
+# all_stations = Station()
+# time = 120 #max minutes
+
+
+# # run 7 trajectories
+# while True:
+#     station = all_stations.get_station()
+#     traject = Connection(station, time)
+    
+#     # allow adding stations as long as the time is not empty
+#     while traject.is_running():
+#         traject.add_connection()
--- stations.py
+++ stations.py
@@ -1,62 +1,134 @@
-"""make sure random function can be used"""
 import random
+import csv
 
 class Station():
-    def __init__(self, name: str, x: float, y: float) -> None:
-        # to do: read csv files with station coördinates
+    def __init__(self, name, x, y) -> None:
         self.name = name
         self.x = x
         self.y = y
 
         self.connections: dict[int: 'Station'] = {}
 
-    def add_csv() -> None:
-        """ add the stations from the csv file """
 
-    def add_station() -> None:
+class Connection():
+    """ Object for connections between two stations. """
+    def __init__(self, station1: str, station2: str, distance: int) -> None:
+        self.station1 = station1
+        self.station2 = station2
+        self.distance = distance
+
+
+class Trajectory():
+    """ Object for a single trajectory. """
+    def __init__(self, station: str, time: int) -> None:
+        self.max_time = time
+        self.time_usage = 0
+        self.current_station = station
+        self.trajectory: list[str] = ["station"]
+
+    def time_left(self) -> int:
+        return self.max_time - self.time_usage
+
+    def time_usage(self) -> int:
+        return self.time_usage
+
+    def add_connection(self, connection: 'Connection') -> None:
+        """ Add a connection to a trajectory. """
+
+        # # Get all connections for the current station
+        # ## DIT WERKT NOG NIET
+        # all_connections = planning.get_connections(self.current_station)
+
+        # # Choose a random connection
+        # new_connection = random.choice(all_connections)
+
+        # add the end station to the trajectory and make it the current station
+        self.trajectory.append(new_connection.station2)
+        self.current_station = new_connection.station2
+
+        # add the distance in time to the usage
+        self.time_usage += new_connection.distance
+
+
+class Planning():
+    def __init__(self) -> None:
+
+        self.stations = []
+
+        # read csv files with station coÃ¶rdinates
+        with open("data/StationsHolland.csv") as file:
+            reader = csv.reader(file)
         
+            # skip the first line
+            next(reader)
+
+            # add each station object to all stations list
+            for name, x, y in reader:
+                station = Station(name, x, y)
+                self.stations.append(station)
+            
+        
+        self.connections = []
+
+        # Open files with all connections
+        with open("data/ConnectiesHolland.csv") as connex:
+            reader_connex = csv.reader(connex)
+            next(reader_connex)
+
+            for station1, station2, distance in reader_connex:
+                # make every connection object twice, for each direction
+                connection = Connection(station1, station2, distance)
+
+                # add connections to the list of all connections
+                self.connections.append(connection)
+                
+
+    def print_stations(self) -> None:
+        """ print all stations. """
+        for station in self.stations:
+            print(f"{station.name}, {station.x}, {station.y}\n")
+
+    def print_all_connections(self) -> None:
+        """ print all connections """
+        for connection in self.connections:
+            print(f"{connection.station1} - {connection.station2}, {connection.distance} min")
+
+    def add_station(self, station: 'Station') -> None:
+        """ Add station to all stations """
+        self.stations.append(station)
+
+
     def get_station() -> str:
         """
-        Choose a random station from the dictionary/list with stations.
+        Choose a random station from the list with stations.
         """
         return random.choice(self.stations)
+
+
+    def create_connection(self, connection: 'Connection') -> None:
+        """ Create a new connection between two stations. """
+        self.connections.append(connection)
+
+
+    def get_connections(self, station: str) -> list['Connection']:
+        list_connections = []
+        for connection in self.connections:
+            if connection.station1 == station or connection.station2 == station:
+                list_connections.append(connection)
+                print(f"{connection.station1} has a connection with {connection.station2} that takes {connection.distance} minutes")
+
+        return list_connections
+
         
-
-
-class Connection(self, station: str, time: int):
-    """ Object for connections between two stations. """
-    def __init__(self) -> None:
-        # to do: read csv file with connections
-        self.station = station
-        self.time = time
-        self.connections: dict[int: 'Station'] = {}
-        file = open('ConnectiesHolland.csv', "r")
-        for line in file:
-            # Save all possible connections in a dict
-
-        file.close()
-        
-    
     def is_running(self) -> bool:
         """check if still enough time left"""  
-        
-    def add_connection(self) -> str:
-        """ 
-        search for a connection with the given station. 
-        Give back the onther station in the connection 
-        make sure to delete the travel time from the total time 
-        """  
-        for line in self.connections:
-            if self.station in line: # and if time in line below total time
-                # add to possebillities 
                 
     def passed(self) -> str:
         """
         list of all stations that are passed
         """   
-        
-        
-        
+
+
     def pattern(self) -> str:
         """ 
         Save the connections
@@ -64,10 +136,8 @@ class Connection(self, station: str, time: int):
         Containing start and finish of each connection and the time passed 
         """
             
-            
+
     def score(self) -> int:
+        """Define score of all trajectories. """
         # K = p*10000 - (T*100 + Min)
         
-        
-        
-
