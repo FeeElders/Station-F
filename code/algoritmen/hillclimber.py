622a8ee0f950012d973e7087ab4d5ba5f6ea33d3
Fee Elders <fee.elders@gmail.com>
2024-06-13 14:21:51 +0200

    starting comments for hillclimber

--- code/algoritmen/hillclimber.py
+++ code/algoritmen/hillclimber.py
@@ -5,9 +5,16 @@ class HillClimber():
         self.new_railway = copy.deepcopy(railway)
 
     def run(self) -> 'Railway':
-        
-        
-    # als een traject minder dan 4 verbindingen heeft vervang je deze 
+        """ change one aspect every run. """
+
+        # TODO: een functie die dit laat runnen tot de score een aantal iteraties niet meer verbetert.
+        # TODO: een aspect elke run veranderen en opnieuw genereren.
+            # bijv: als een traject minder dan 4 verbindingen heeft vervang je deze, alleen stopt dit vrij snel wss.
+            # andere optie: elke keer het kleinste traject eruit halen en vervangen
+            # andere optie: kleinste trajct eruit halen, dan vergelijken of de score beter wordt en behouden of vervangen. Dan pas kiezen of er ook een nieuw traject wordt aangemaakt, misschien wel trajecten blijven verwijderen tot de score niet meer beter wordt. dan komen we misschien op een gemiddeld aantal trajecten dat optimaal is.
+        ## De oude railway + score bewaren in een variabele
+        ## Deze vergelijken met de nieuwe score en dan vervangen of behouden
+
     
     
         
