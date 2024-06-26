e9c2b3c1ffa2c15df4ff8cd129a52fe3f1229b59
Fee Elders <fee.elders@gmail.com>
2024-06-26 15:13:09 +0200

    start with command line arguments

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 import csv
 from csv import writer
 import copy
+import sys, argparse, getopt
 
 from code import helpers
 from code.classes import station, railway, connection, trajectory
@@ -16,62 +17,49 @@ from experimenten import greedy_experiment
 from datetime import datetime
 
 if __name__ == "__main__":
-    print("WELCOME TO RailNL")
-    
-    data = "nl"
-    
-    if data == "nl":
+    # Initialize parser
+    parser = argparse.ArgumentParser()
+
+    # Adding optional argument
+    parser.add_argument("-m", "--map", default = "nl", choices = ["nl", "nh"], help = "choose which data you want to use")
+    parser.add_argument("-a", "--algoritme", default = "random", choices = ["Random", "Greedy", "HillClimber"], help = "Choose algorithm")
+    parser.add_argument("-ex", "--experiment", default = "bs", help = "Do Trajectory Amount Experiment")
+
+    # Read arguments from command line
+    args = parser.parse_args()
+
+    if args.map == "nl":
         time = 180
         max_trajectories = 20
         stations_csv = "StationsNationaal"
         connections_csv = "ConnectiesNationaal"
-        
-    elif data == "nh":
+
+    if args.map == "nh":
         time = 120
         max_trajectories = 7
         stations_csv = "StationsHolland"
-        connections_csv = "ConnectiesHolland"
-
+        connections_csv = "ConnectiesHolland"        
+    
     railway = railway.Railway(max_trajectories, time)
     railway.load_stations(f"data/{stations_csv}.csv")
     railway.load_connections(f"data/{connections_csv}.csv")
 
-    
- 
-    # --------------------Random----------------------------------
-    
-    # random_experiment.trajectory_amount(railway, max_trajectories)
-    # random_experiment.notsorandom_trajectory_amount(railway, max_trajectories)
-    # random_experiment.baseline(railway, max_trajectories)
-#     random_experiment.max_traject(railway, max_trajectories)
-#     random_experiment.no_visited_connections_max(railway, max_trajectories)
-#     random_experiment.no_visited_connections_random(railway, max_trajectories)
-#     random_experiment.not_so_random(railway, max_trajectories)
-    
+
+    # initialize dicionary with all experiments
+    random_dict{"baseline": "random_experiment.baseline",
+                "max_traject": "random_experiment.max_traject",
+                "nvc_max": "random_experiment.no_visited_connections_max",
+                "nvc_random": "random_experiment.no_visited_connections_random",
+                "not_so_random": "random_experiment.not_so_random"}
+                
     
 
-    # ------------------------------HillClimber-------------------------------
-
-    #hillclimber_experiment.hillclimb_traject_amount(railway, max_trajectories)
+    print("WELCOME TO RailNL")
     
-    # hillclimber_experiment.hillclimb_traject_amount_smart(railway, max_trajectories)
-    # hillclimber_experiment.hillclimb_4_2(railway, max_trajectories)
-#     max_trajectories = 11
-#     hillclimber_experiment.hillclimb(railway, max_trajectories)
-#     hillclimber_experiment.hillclimb_noreturn(railway, max_trajectories)
-#     hillclimber_experiment.hillclimb_smart_start(railway, max_trajectories)
-#     hillclimber_experiment.smart_remove(railway, max_trajectories)
-
-    hillclimber_experiment.greedy_over_hill("HillClimber-smart_start_26-06-2024", "Smart Hill met greedy", 6592)
-    # ------------------------------------Greedy------------------------------
-    # greedy_experiment.trajectory_amount(railway, max_trajectories)
-    # greedy_experiment.smart_trajectory_amount(railway, max_trajectories)
-    # greedy_experiment.greedy(railway, max_trajectories)
-    # greedy_experiment.smart_greedy(railway, max_trajectories)
-    # greedy_experiment.random_greedy(railway, max_trajectories)
+    if args.algoritme:
+        print(f"Running {args.algoritme} algorithm.")
+        if args.algoritme == "random" and args.experiment == "bs":
+            random_dict["baseline"](railway)
+            
     
-    # greedy_long = gr.Greedy(railway)
-#     gr_long = greedy_long.run(20)
-#     print(f"greedy score: {gr_long.score()}")
-
-
+      
